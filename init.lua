c387e62c151cda0d57798337c5f138790aaa4089
qudroturkey <reesemb1@gmail.com>
2025-11-12 23:34:50 -0600

    trying to fix linter

--- init.lua
+++ init.lua
@@ -782,7 +782,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        ruby = { 'rubocop' },
+        -- ruby = { 'rubocop' },
         javascript = { 'prettier' },
 
         -- Conform can also run multiple formatters sequentially
